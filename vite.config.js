1480e2ccd9f40b5fa43058cca1e0d92d145a69dc
kriti211 <kritisodani220667@acropolis.in>
2025-11-17 13:59:01 +0530

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Kriti_Portfolio/'   // ⚠️ write your repo name EXACTLY
 })
