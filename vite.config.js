dd502d5317a8408d486e70c596c9cb1b20e8e699
kriti211 <kritisodani220667@acropolis.in>
2025-11-17 14:09:59 +0530

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Kriti_Portfolio/'   // ⚠️ write your repo name EXACTLY
+  base: '/Frontend/',   // <-- your GitHub repo name
 })
